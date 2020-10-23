e03a9f6f809256946e7d0e8b112c9adbab772b8e
James Gopsill <james.gopsill@kadlytics.com>
2020-10-23 11:24:35 +0100

    Documentation updates and .gitkeep empty out folder

--- src/main.py
+++ src/main.py
@@ -9,23 +9,25 @@ if __name__ == "__main__":
 	# 3D printer stl file
 	file_name = "stls/solid_beam.stl"
 
+	# Read in the STL
 	stl_file = vtk.vtkSTLReader()
 	stl_file.SetFileName(file_name)
 	stl_file.Update()
 	stl_poly = stl_file.GetOutput()
 
+	# Get the min/max bounds for the STL
 	x_min, x_max, y_min, y_max, z_min, z_max = stl_poly.GetBounds()
 	print("X min:", x_min, "X max:", x_max, "Y min:", y_min, "Y max:", y_max, "Z min:", z_min, "Z max:", z_max)
 
-	# generate internal mesh structure of stl
+	# Generate internal mesh structure of stl
 	stl_tree = vtk.vtkOBBTree()
 	stl_tree.SetDataSet(stl_poly)
 	stl_tree.BuildLocator()
 
-	# Set a slice level and mesh width
+	# Set a slice level
 	level = ( z_min + z_max ) / 2
 
-	# Create intersecting line
+	# Create intersecting plane
 	plane = vtk.vtkPlane()
 	plane.SetOrigin(0, 0, level)
 	plane.SetNormal(0, 0, 1)
@@ -36,16 +38,20 @@ if __name__ == "__main__":
 	cutter.SetInputConnection(stl_file.GetOutputPort())
 	cutter.Update()
 
-	# get the lines from the cutter
+	# Get the lines from the cutter
 	cut_strips = vtk.vtkStripper()
 	cut_strips.SetInputConnection(cutter.GetOutputPort())
 	cut_strips.Update()
 
-	# create polys from the lines
+	# Create polys from the lines
 	cut_polys = vtk.vtkPolyData()
 	cut_polys.SetPoints(cut_strips.GetOutput().GetPoints())
 	cut_polys.SetPolys(cut_strips.GetOutput().GetLines())
 
+	# Generate the outer shells
+	# TODO: create the ability to return internal shells
+	# Return as outer_shells, internal_shell = generate_shells()
+	# Where outer_shells is a list of lines for the outer_shells and internal_shells is a list of lists that would describe each internal shell and series of paths associated with it
 	shells = generate_shells(cut_polys, 3)
 
 	# Select the inner shells
@@ -53,6 +59,8 @@ if __name__ == "__main__":
 	mesh_width = 0.4
 	linear_x_lines = linear_mesh.linear_x(shells[-1], x_min-10, x_max+10, y_min-10, y_max+10, mesh_width)
 	linear_y_lines = linear_mesh.linear_y(shells[-1], x_min-10, x_max+10, y_min-10, y_max+10, mesh_width)
+
+	# Draw out the print lines
 
 	c = pyx.canvas.canvas()
 	for shell in shells:
@@ -70,3 +78,5 @@ if __name__ == "__main__":
 		c.stroke(line, [pyx.style.linewidth(0.05), pyx.color.rgb.blue])
 
 	c.writePDFfile("out/shells+linear_x+linear_y.pdf")
+
+	# TODO: Create a Gcode generator given the paths that want to be printed.
